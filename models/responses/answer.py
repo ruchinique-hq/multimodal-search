aef31c476ad76b73c93cef18c1b0b8892476d93a
Ashwini <ashwini@ruchinique.com>
2024-12-18 01:01:37 +0530

    added function to convert answer object to json

--- models/responses/answer.py
+++ models/responses/answer.py
@@ -5,3 +5,9 @@ class SearchAnswerResponse:
     def __init__(self, query: str, answer: str):
         self.query = query
         self.answer = answer
+
+    def to_json(self):
+        return {
+            "query": self.query,
+            "answer": self.answer
+        }
